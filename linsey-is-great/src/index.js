9275e8486d80ff064ddf2e3ca0f31479d896c48c
jacksorjacksor <jacksorjacksor@pm.me>
2022-02-09 11:48:52 +0000

    pre-examples

--- linsey-is-great/src/index.js
+++ linsey-is-great/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 class Square extends React.Component {
+// State
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => console.log('click')}>
+        {this.props.value}
       </button>
     );
   }
@@ -43,13 +51,31 @@ class Board extends React.Component {
   }
 }
 
+class Lins extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Lins is { this.props.value }</p>
+      </div>
+    )
+  }
+}
+
+
+
 class Game extends React.Component {
+  linsFunction(i){
+    return <Lins value = {i} />
+  }
+
   render() {
     return (
       <div className="game">
+        { this.linsFunction("variable")}
         <div className="game-board">
           <Board />
         </div>
+        
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
